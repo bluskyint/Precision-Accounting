dc44b8de302827cd1860071a1ba6b793ff5077d3
Dev-ismael <dev.ismael305@gmail.com>
2023-09-21 11:52:57 +0300

    Update Slider imgs

---
+++ resources/views/static/terms.blade.php
@@ -0,0 +1,26 @@
+
+@extends('layouts.app')
+
+@section('pageUrl', 'terms')
+
+@section('content')
+
+    <div class="main-content pt--125">
+
+        <div class="rwt-contact-area rn-section-gap">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 mb--40">
+                        <div class="section-title text-center"  data-sal="slide-down" data-sal-duration="700" data-sal-delay="300" data-sal-easing="ease-out-back">
+                            <h2 class="title w-600 mb--20">Our Terms & Conditions</h2>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+
+@endsection
